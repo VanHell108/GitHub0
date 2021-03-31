6698fceada1630a9bac0d999d2c734437d1582e9
PC <PC@DESKTOP-KSMLL6F>
2021-04-01 00:46:57 +0300

    Commit comment

---
+++ Java/src/my/User.java
@@ -0,0 +1,24 @@
+package my;
+
+public class User {
+	public String newName;
+
+    public String getName1() {
+        return newName;
+    }
+
+    public void setName1(String name) {
+        this.newName = name;
+    }
+
+
+    public String myName;
+
+    public String getName() {
+        return myName;
+    }
+
+    public void setName(String name) {
+        this.myName = name;
+    }
+}
